32b59b1732b4f8e807e51f1b5488337f43653b7d
C Nelson <cnelsonsic@gmail.com>
2010-12-06 10:50:50 -0600

    Add a definable resources directory setting, a data writing delay for time-based dumping, and cleaned up some comments.

--- settings.py
+++ settings.py
@@ -1,20 +1,37 @@
 '''
 A note about settings:
 The number sign: "#" is the mark to denote a line is a comment.
-By commenting out lines, you can turn them off and return them to their defaults.
-This is Python, so dont add whitespace that isnt there, use proper capitalization.
+By commenting out lines, you can turn them off and return them to the defaults.
+This is Python, dont add whitespace that isnt there, use proper capitalization.
 In general, follow the examples.
 '''
  
 #Set one or more of these to True to get more units of measurement.
-#If you disable one of these, their units wont go away until you delete your database file.
+#If you disable one of these, their units wont go away until 
+#you delete your database file.
 METRIC = True
 IMPERIAL = False
 UNITEDSTATES = False
 
-TRAY_ICON = "resources/bottle.png"
-DATABASE_PATH = "sqlite:///liquor_cabinet.sqlite"
+RESOURCES_DIR = "resources/"
+TEMPLATE_DIR = RESOURCES_DIR+"templates/"
+
+TRAY_ICON = RESOURCES_DIR+"bottle.png"
+DATABASE_PATH = "sqlite:///:memory:"
 DEBUG = False
+WRITE_DATA_DELAY = 1000*60*5 #Time between writing data to disk In milliseconds
+
+SPLASH_MESSAGE_DELAY = 0.333
+NOSPLASH = True #Disables splash screen entirely.
+
+#10 hours past 8am, 6pm
+PURITAN_TIME = 10
+
+#4 hours past 8am, 12 noon
+MODERN_TIME = 4 
+
+#A sure sign of alcoholism is when a person is drunk before noon. Or 6pm.
+DRUNK_BEFORE_NOON = PURITAN_TIME 
 
 LOADING_MESSAGES = (
     "Extruding Mesh Terrain",
